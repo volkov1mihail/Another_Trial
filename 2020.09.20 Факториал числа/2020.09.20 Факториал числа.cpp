bc8f0fb6be83ffe527c5f51efc905de8c13efd66
newaccount113377 <volkov1mihail@gmail.com>
2020-09-21 00:00:45 +0300

    Create 2020.09.20 Факториал числа.cpp

---
+++ 2020.09.20 Факториал числа/2020.09.20 Факториал числа.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+	int n = 0; //¬водимое число
+	int f = 1; //‘акториал вводимого числа
+	cin >> n;
+	if (n == 0)
+	{
+		cout << 0 << endl;
+	}
+	else
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			f = f * i;
+		}
+		cout << "f=" << f << endl;
+	}
+	return EXIT_SUCCESS;
+}
