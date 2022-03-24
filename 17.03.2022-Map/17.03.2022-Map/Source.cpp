6c4ecb28df91a2829cfa1bda3da3ef8634bab015
newaccount113377 <volkov1mihail@gmail.com>
2022-03-24 12:25:24 +0300

    d

--- 17.03.2022-Map/17.03.2022-Map/Source.cpp
+++ 17.03.2022-Map/17.03.2022-Map/Source.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<map>
+#include<string>
 using namespace std;
 
 void print(const map<int, int>& M)
@@ -7,6 +8,23 @@ void print(const map<int, int>& M)
 	for (auto x : M)
 		cout << x.first << " -> " << x.second << endl;
 }
+
+
+map<int, int> toInt()
+{
+	int a = 0;
+	int c = 0;
+	map<int, int> m;
+	cout << "Choose the nubmer of pairs" << endl;
+	cin >> c;;
+	for (int j = 0; j < c; ++j)
+	{
+		cin >> a;
+		cin >> m[a];
+	}
+	return m;
+}
+
 
 
 map<int, int> count()
@@ -21,8 +39,52 @@ map<int, int> count()
 }
 
 
+map<int, int> second()
+{
+	map<int, int> m = toInt();
+	int c = -1;
+	while (c != 0)
+	{
+		cin >> c;
+		if (m.count(c) == 0) cout << c << endl;
+		else cout << m[c] << endl;
+	}
+	return m;
+}
+
+template<class T, class P>
+map<T, P> sum(map<T, P> m1, map<T, P> m2)
+{
+	map<T, P> result;
+	for (auto x : m1)
+	{
+		result[x.first] = x.second + m2[x.first];
+	}
+	return result;
+}
+
+
+template <class T1, class T2, class T3>
+map<T1, T3> composition(map<T1, T2> m1, map<T2, T3> m2)
+{
+	map<T1, T3> result;
+	for (auto x : m1)
+	{
+		result[x.fisrt] = m2[x.second];
+	}
+	return result;
+}
+
+
 int main()
 {
-	print(count());
+	//print(count());
+
+	map<int, int> m1 = toInt();
+	map<int, int> m2 = toInt();
+	map<int, int> m = composition(m1, m2);
+	print(m);
+	//map<int, int> m = second();
+
 	return 0;
 }
---
+++ 2022.03.24 - Parallel_Sections/2022.03.24 - Parallel_Sections/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <omp.h>
+#include <cmath>
+#include<iomanip>
+
+#define N 100000000
+double PI = 3.14159265;
+double x[100000000];
+
+
+using namespace std;
+int main()
+{
+	srand(time(0));
+	return 0;
+}
