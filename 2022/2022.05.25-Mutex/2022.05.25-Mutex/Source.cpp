947059cb868509d577007e09a9ed6f110fcec3f9
newaccount113377 <volkov1mihail@gmail.com>
2024-08-28 16:34:07 +0300

    Mutex

--- 2022/2022.05.25-Mutex/2022.05.25-Mutex/Source.cpp
+++ 2022/2022.05.25-Mutex/2022.05.25-Mutex/Source.cpp
@@ -2,146 +2,151 @@
 #include<iostream>
 #include<thread>
 #include<mutex>
-#include<vector>
-#include<omp.h> 
-#include<chrono>
 #include<queue>
 #include<condition_variable>
 using namespace std;
+//Программа имитирует взаимодействие завода(factory) и потребителя(consumer)
+//Если на складе(в очереди q) отсутствует товар(очередь пуста), завод передает одну единицу товара на склад
+//Если на складе есть товар, потребитель забирает товар со склада
+//Одновременно в работу запускается 2 потока, выполняющих должности завода, и 3 потока, выполняющих должности потребителя
+//Исследуется поведение программы в случае использования библиотеки mutex, condition_variable и только с библиотекой tread
 
-condition_variable cv;
-int object;
-mutex m;
-queue < int> q;
-bool flag = false;
-bool work1 = true, work2 = true;
-vector<int> v1, v2;
+//Генерация случайных чисел, играющих роль товара
 mt19937 mt;
 uniform_int_distribution<int> r(0, 100);
 
+bool flag = false;													//Индикатор того, заполнен ли склад
+bool work1 = true; bool work2 = true;				//Индикаторы того, идет ли работу по заполнению(work1) и опустошению склада(work2)
+
+mutex m;
+queue<int> q;																//Склад
+condition_variable cv;
+
+//Функции, не использующие mutex и conditional_variable
 void factory();
 void consumer();
 void check_all();
+
+//Функции, использующие mutex
 void factory_m();
 void consumer_m();
 void check_all_m();
+
+//Функции, использующие mutex и conditional_variable
+bool is_full() { return !q.empty(); }
 void factory_cv();
 void consumer_cv();
 void check_all_cv();
-void factory_cvm();
-void consumer_cvm();
-void check_all_cvm();
-void factory_3();
-void consumer_3();
-void check_all_3();
 
-
-bool f()
+int main()
 {
-	return !q.empty();
+	srand(time(0));
+
+	//check_all();
+	//work1 = true;
+	//work2 = true;
+	//flag = false;
+	//system("pause");
+
+	check_all_m();
+	work1 = true;
+	work2 = true;
+	flag = false;
+	system("pause");
+
+	check_all_cv();
+
+	return 0;
 }
 
 
-void factory()
+void factory()											//Функция, добавляющая в очередь элемент в случае, если она пуста
 {
 	while (work1)
 	{
-		if (flag == true) { this_thread::sleep_for(chrono::microseconds(r(mt))); }
+		if (!q.empty())
+			this_thread::sleep_for(chrono::microseconds(r(mt)));
 		else
-		{
-			object = r(mt);
-			v1.push_back(object);
-			flag = true;
-		}
+			q.push(r(mt));
 	}
 }
 
-
-void consumer()
+void consumer()											//Функция, удаляющая из очереди элемент в случае, если она не пуста
 {
 	while (work2)
 	{
-		if (flag == false) this_thread::sleep_for(chrono::microseconds(r(mt)));
+		if (q.empty())
+			this_thread::sleep_for(chrono::microseconds(r(mt)));
 		else
-		{
-			v2.push_back(object);
-			flag = false;
-		}
+			q.pop();
 	}
 }
 
-
-void check_all()
+void check_all()										//Функция, запускающая несколько потоков, одновременно имеющих доступ к очереди q
 {
+	//Потоки, пополняющие очередь
 	thread f1(factory);
 	thread f2(factory);
+	//Потоки, опусташающие очередь
 	thread c1(consumer);
 	thread c2(consumer);
 	thread c3(consumer);
-	this_thread::sleep_for(1s);
-	work1 = false;
+	this_thread::sleep_for(1s);				//Время наблюдения за работой потоков
+
+	work1 = false;										//Остановка потоков
 	f1.join();
 	f2.join();
 	work2 = false;
 	c1.join();
 	c2.join();
 	c3.join();
-	cout << "v1=";
-	for (int i = 0; i < v1.size(); i++) {
-		cout << v1[i] << " ";
+
+	cout << "q = ";										//Проверка, остались ли какие-то элементы в очереди(очередь должна быть пуста)
+	while (!q.empty())
+	{
+		cout << q.front() << " ";
+		q.pop();
 	}
 	cout << endl;
-	cout << "v2=";
-	for (int i = 0; i < v2.size(); i++) {
-		cout << v2[i] << " ";
-	}
-	cout << endl;
-	v1 == v2 ? cout << "v1 = v2" << endl : cout << "v1 != v2" << endl;
 }
 
-
-void factory_m()
+void factory_m()										//Функция, добавляющая в очередь элемент в случае, если она пуста(с использованием mutex)
 {
 	while (work1)
 	{
 		m.lock();
-		if (flag == true)
+		if (!q.empty())
 		{
 			m.unlock();
 			this_thread::sleep_for(chrono::microseconds(r(mt)));
 		}
 		else
 		{
-			object = r(mt);
-			v1.push_back(object);
-			flag = true;
+			q.push(r(mt));
 			m.unlock();
 		}
 	}
 }
 
-
-void consumer_m()
+void consumer_m()										//Функция, удаляющая из очереди элемент в случае, если она не пуста(с использованием mutex)
 {
 	while (work2)
 	{
 		m.lock();
-		if (flag == false)
+		if (q.empty())
 		{
 			m.unlock();
 			this_thread::sleep_for(chrono::microseconds(r(mt)));
 		}
 		else
 		{
-			v2.push_back(object);
-			flag = false;
+			q.pop();
 			m.unlock();
 		}
 	}
 }
 
-
-void check_all_m()
+void check_all_m()									//Функция, запускающая несколько потоков, одновременно имеющих доступ к очереди q(с использованием mutex)
 {
 	thread f1(factory_m);
 	thread f2(factory_m);
@@ -149,6 +154,7 @@ void check_all_m()
 	thread c2(consumer_m);
 	thread c3(consumer_m);
 	this_thread::sleep_for(1s);
+
 	work1 = false;
 	f1.join();
 	f2.join();
@@ -156,157 +162,17 @@ void check_all_m()
 	c1.join();
 	c2.join();
 	c3.join();
-	cout << "v1=";
-	for (int i = 0; i < v1.size(); i++) {
-		cout << v1[i] << " ";
-	}
-	cout << endl;
-	cout << "v2=";
-	for (int i = 0; i < v2.size(); i++) {
-		cout << v2[i] << " ";
-	}
-	cout << endl;
-	v1 == v2 ? cout << "v1 = v2" << endl : cout << "v1 != v2" << endl;
-}
 
-
-int main()
-{
-	srand(time(0));
-	//check_all();
-	//check_all_m();
-	//check_all_cv();
-	check_all_cvm();
-	//check_all_3();
-	return 0;
-}
-
-
-void factory_cv()
-{
-	while (work1)
-	{
-		if (!q.empty())
-			this_thread::sleep_for(chrono::microseconds(r(mt)));
-
-		else q.push(r(mt));
-	}
-}
-
-
-void consumer_cv()
-{
-	while (work2)
-	{
-		if (q.empty())
-			this_thread::sleep_for(chrono::microseconds(r(mt)));
-
-		else
-		{
-			cout << q.front() << " ";
-			q.pop();
-		}
-	}
-}
-
-
-void check_all_cv()
-{
-	thread f1(factory_cv);
-	thread f2(factory_cv);
-	thread c1(consumer_cv);
-	thread c2(consumer_cv);
-	thread c3(consumer_cv);
-	this_thread::sleep_for(1s);
-	work1 = false;
-	f1.join();
-	f2.join();
-	work2 = false;
-	c1.join();
-	c2.join();
-	c3.join();
-	cout << "q= ";
+	cout << "q = ";
 	while (!q.empty())
 	{
-		int val = q.front();
+		cout << q.front() << " ";
 		q.pop();
-		cout << val << " ";
 	}
 	cout << endl;
 }
 
-
-void factory_cvm()
-{
-	while (work1)
-	{
-		m.lock();
-		if (!q.empty())
-		{
-			m.unlock();
-			this_thread::sleep_for(chrono::microseconds(r(mt)));
-		}
-		else
-		{
-			int c = r(mt);
-			q.push(c);
-			v1.push_back(c);
-			m.unlock();
-		}
-	}
-}
-
-
-void consumer_cvm()
-{
-	while (work2)
-	{
-		m.lock();
-		if (q.empty())
-		{
-			m.unlock();
-			this_thread::sleep_for(chrono::microseconds(r(mt)));
-		}
-		else
-		{
-			v2.push_back(q.front());
-			q.pop();
-			m.unlock();
-		}
-	}
-}
-
-
-void check_all_cvm()
-{
-	thread f1(factory_cvm);
-	thread f2(factory_cvm);
-	thread c1(consumer_cvm);
-	thread c2(consumer_cvm);
-	thread c3(consumer_cvm);
-	this_thread::sleep_for(1s);
-	work1 = false;
-	f1.join();
-	f2.join();
-	work2 = false;
-	c1.join();
-	c2.join();
-	c3.join();
-	cout << "v1=";
-	for (int i = 0; i < v1.size(); i++) {
-		cout << v1[i] << " ";
-	}
-	cout << endl;
-	cout << "v2=";
-	for (int i = 0; i < v2.size(); i++) {
-		cout << v2[i] << " ";
-	}
-	cout << endl;
-	v1 == v2 ? cout << "v1 = v2" << endl : cout << "v1 != v2" << endl;
-}
-
-
-void factory_3()
+void factory_cv()										//Пополнение очереди, испольуя mutex и conditional_variable
 {
 	while (work1)
 	{
@@ -319,7 +185,6 @@ void factory_3()
 		else
 		{
 			q.push(r(mt));
-			v1.push_back(q.front());
 			cv.notify_one();
 			this_thread::sleep_for(chrono::milliseconds(r(mt)));
 		}
@@ -327,35 +192,32 @@ void factory_3()
 	cv.notify_all();
 }
 
-
-void consumer_3()
+void consumer_cv()									//Опустошение очереди, испольуя mutex и conditional_variable
 {
 	while (work2)
 	{
 		unique_lock <mutex> l(m);
-		cv.wait(l, f);
+		cv.wait(l, is_full);
 		if (q.empty()) return;
 		else
 		{
-			v2.push_back(q.front());
 			q.pop();
 			cv.notify_all();
 			return;
 		}
-		v2.push_back(q.front());
 		q.pop();
 	}
 }
 
-
-void check_all_3()
+void check_all_cv()								//Запуск потоков, использующих mutex и conditional_variable
 {
-	thread f1(factory_3);
-	thread f2(factory_3);
-	thread c1(consumer_3);
-	thread c2(consumer_3);
-	thread c3(consumer_3);
+	thread f1(factory_cv);
+	thread f2(factory_cv);
+	thread c1(consumer_cv);
+	thread c2(consumer_cv);
+	thread c3(consumer_cv);
 	this_thread::sleep_for(1s);
+
 	work1 = false;
 	f1.join();
 	f2.join();
@@ -363,10 +225,12 @@ void check_all_3()
 	c1.join();
 	c2.join();
 	c3.join();
-	cout << endl;
-	v1 == v2 ? cout << "v1 = v2" << endl : cout << "v1 != v2" << endl;
-	cout << "v2= ";
-	for (int i = 0; i < v2.size(); i++) {
-		cout << v2[i] << " ";
+
+	cout << "q = ";
+	while (!q.empty())
+	{
+		cout << q.front() << " ";
+		q.pop();
 	}
+	cout << endl;
 }
